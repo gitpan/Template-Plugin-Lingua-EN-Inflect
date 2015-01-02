97ce606b875f19015555b4a231afa1f1a1866ba1
Barbie <barbie@missbarbell.co.uk>
2015-01-02 10:06:20 +0000

    Import of BARBIE/Template-Plugin-Lingua-EN-Inflect-0.04.tar.gz from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Lingua-EN-Inflect
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BARBIE/Template-Plugin-Lingua-EN-Inflect-0.04.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/Lingua/EN/Inflect.pm
+++ lib/Template/Plugin/Lingua/EN/Inflect.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = 0.03;
+$VERSION = 0.04;
 
 require Template::Plugin;
 use base qw(Template::Plugin);
@@ -281,7 +281,7 @@ Lingua::EN::Inflect module, which does all the clever stuff.
 =head1 COPYRIGHT & LICENSE
 
 Copyright (C) 2005-2014 Andrew Ford
-Copyright (C) 2014      Barbie for Miss Barbell Productions.
+Copyright (C) 2014-2015 Barbie for Miss Barbell Productions.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the Artistic Licence v2.
